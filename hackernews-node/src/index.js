e39b89450f03670258fc034143424e079b85ad2b
Federico Toledo <federicotoledo@sirius.com.ar>
2021-03-10 13:41:40 -0300

    initial commit

---
+++ hackernews-node/src/index.js
@@ -0,0 +1,41 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+        link: id => links.find(link => link.id === id)
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        },
+        deleteLink: (parent, args) => {
+            console.log(args);
+            let idNumber = args.id.replace(/^\D+/g, '')
+            console.log(idNumber);
+            let current = links.splice(idNumber, 1)
+            console.log(current);
+        }
+    },
+}
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+})
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
